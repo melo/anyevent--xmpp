19efd1cb77590a1060d4d37195af089d1c774ba6
Pedro Melo <melo@simplicidade.org>
2009-09-05 18:24:50 +0100

    Make use of the AnyEvent::XMPP::Ext::DataForm
    This module already parses data form nodes and provides the correct
    verification string per form.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/AnyEvent/XMPP/Ext/Caps.pm
+++ lib/AnyEvent/XMPP/Ext/Caps.pm
@@ -6,6 +6,7 @@ use base qw/AnyEvent::XMPP::Ext/;
 
 use AnyEvent::XMPP::Namespaces qw/ set_xmpp_ns_alias /;
 use AnyEvent::XMPP::Util qw/ split_jid prep_join_jid resourceprep /;
+use AnyEvent::XMPP::Ext::DataForm;
 use MIME::Base64 qw( encode_base64 );
 use Digest::SHA1 qw( sha1 );
 
@@ -18,13 +19,10 @@ set_xmpp_ns_alias(caps => 'http://jabber.org/protocol/caps');
 
 sub _ver_gen {
   my ($disco_info, $hash) = @_;
-  ### FIXME: add support for XEP-0128
 
-  my ($ids, $features, $forms) = _extract_idents_and_features($disco_info);
-  map { $_ = "$_->{category}/$_->{type}/$_->{lang}/$_->{name}" } @$ids;
-  _serialize_forms($forms);
+  my ($ids, $features, $forms) = _extract_ids_features_and_forms($disco_info);
 
-  my $S = join('<', sort @$ids, sort @$features, sort @$forms) . '<';
+  my $S = join('', sort @$ids, sort @$features, sort @$forms);
   utf8::encode($S);    ### Turns UTF-8 string into byte sequence
 
   if ($hash eq 'sha-1') {
@@ -37,58 +35,32 @@ sub _ver_gen {
   return encode_base64($S, '');
 }
 
-sub _extract_idents_and_features {
+sub _extract_ids_features_and_forms {
   my $di = shift;
 
   my (@ids, @feats, @forms);
   foreach my $node ($di->nodes) {
     if ($node->eq('disco_info', 'identity')) {
       push @ids,
-        {
-        category => $node->attr('category') || '',
-        name     => $node->attr('name')     || '',
-        type     => $node->attr('type')     || '',
-        lang => $node->attr_ns('xml', 'lang') || '',
-        };
+        join(
+        '/',
+        map { $_ || '' } (
+          $node->attr('category'), $node->attr('type'),
+          $node->attr_ns('xml', 'lang'), $node->attr('name')
+        ),
+        ) . '<';
     }
     elsif ($node->eq('disco_info', 'feature')) {
-      push @feats, $node->attr('var');
+      push @feats, $node->attr('var') . '<';
     }
     elsif ($node->eq('data_form', 'x')) {
-      push @forms, $node;
+      my $df = AnyEvent::XMPP::Ext::DataForm->new;
+      $df->from_node($node);
+      push @forms, $df->as_verification_string;
     }
   }
 
   return (\@ids, \@feats, \@forms);
-}
-
-sub _serialize_forms {
-  my $forms = shift;
-
-  for my $form (@$forms) {
-    my @S;
-    my $type;
-    my @fields =
-      sort { $a->attr('var') cmp $b->attr('var') }
-      $form->find(qw/data_form field/);
-      
-    for my $field (@fields) {
-      my $var = $field->attr('var');
-      my @values =
-        sort map { $_->text } $field->find(qw/data_form value/);
-
-      if ($var eq 'FORM_TYPE') {
-        $type = $values[0];
-      }
-      else {
-        push @S, join('<', $var, @values);
-      }
-    }
-
-    $form = join('<', $type,@S);
-  }
-  
-  return;
 }
 
 1;
